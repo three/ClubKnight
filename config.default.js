e308b1ce0a359c0bfcf217a08c962b52f4b47eb5
Eric Roberts <eric@threedot14.com>
2017-02-28 21:04:07 -0500

    Send validation emails through sendgrid
    If a sendgrid token isn't specified, emails will not be sent.

--- config.default.js
+++ config.default.js
@@ -5,5 +5,11 @@ module.exports = {
     database: path.join(__dirname,"data/persistence.sqlite"),
 
     // SendValidations
-    sendgrid_apikey: "", // Log tokens instead of sending them
+    sendgrid_apikey: "",
+    email_basedomain: "scarletmail.rutgers.edu",
+    email_from: "noreply@example.org",
+
+    // Validation Email
+    email_valsubject: "[ClubKnight] Please confirm you're a Rutgers student",
+    email_valbody: "Your validation token is %%VALTOKEN%%. Please go to example.org/validate.html",
 };
