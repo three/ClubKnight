ee740ac81d9f9fa35bceaf9fb4802f88068d6d6e
Eric Roberts <eric@threedot14.com>
2017-03-07 11:38:31 -0500

    Fix ESLint errors

--- static/js/global.js
+++ static/js/global.js
@@ -4,7 +4,7 @@
  * Global functions, and check browser support. Avoid modern javascript
  * features.
  */
-/* eslint-env browser */
+/* eslint-disable */
 
 var badSupportDialog;
 
@@ -107,6 +107,7 @@ function initDynamicForm(form) {
                 console.log("error", revt);
                 sbmter.disabled = false;
 
+                var resText;
                 for (var i=0;i<responseFields.length;i++) {
                     responseFields[i].innerHTML = "";
                     resText = document.createTextNode(
